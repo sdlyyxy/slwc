e476ba4c2b5215ee7bf9848e590a021189100f2e
1 <DxxK@MrK.local>
2017-04-11 16:28:10 +0800

    2017年 4月11日 星期二 16时28分10秒 CST

--- Show_Result.php
+++ Show_Result.php
@@ -15,12 +15,10 @@
 
 	$xml=simplexml_load_file("Qustion_Data_Base87373a3c11a6e866f2aa56f6ab9be8d4.xml");
 	$stdAns=$xml->$question_id;
-
 	if($stdAns==$answer)
 	{
         //update student attribute
         $student_json_name=$student_id.".json";
-        $student_json_file;
         if(file_exists($student_json_name))
         {
             $student_json_file=fopen($student_json_name,'r');
@@ -49,17 +47,6 @@
 
 		if($student_ansflag==0)//haven't answer right yet
         {
-            //update answer times
-            //echo "   in";
-            $student_ansflag=1;
-            $student_obj->{$question_id}=$student_ansflag;
-            $student_outData=json_encode($student_obj);
-            $student_outData.="\n";
-            $student_json_file=fopen($student_json_name,'w');
-            fwrite($student_json_file,$student_outData);
-            fclose($student_json_file);
-
-
 
             $sqlFile = fopen("QustionAnswerTimes.json", 'r');
             $data = fgets($sqlFile);
@@ -75,6 +62,30 @@
             $sqlFile = fopen("QustionAnswerTimes.json", 'w');
             fwrite($sqlFile, $outData);
             fclose($sqlFile);
+
+
+            //update answer times
+            //echo "   in";
+            $student_ansflag=1;
+            $student_obj->{$question_id}=$student_ansflag;
+            //update Correct_Times
+            $student_Correct_Times=(int)$student_obj->{"Correct_Times"};
+            $student_Correct_Times=$student_Correct_Times+1;
+            $student_obj->{"Correct_Times"}=$student_Correct_Times;
+            //update rank
+            $student_Rank=(int)$student_obj->{"Rank"};
+            $student_Rank=$student_Rank+41-$tempInt;
+            $student_obj->{"Rank"}=$student_Rank;
+
+            $student_outData=json_encode($student_obj);
+            $student_outData.="\n";
+            $student_json_file=fopen($student_json_name,'w');
+            fwrite($student_json_file,$student_outData);
+            fclose($student_json_file);
+
+
+
+
 
 
             $right_Content=file_get_contents("right.html");
@@ -94,10 +105,26 @@
 
 
 	else
-	{
-        $false_Content=file_get_contents('wrong.html');
-        echo $false_Content;
-		$outPut.="No";
+    {
+        $student_json_name=$student_id.".json";
+        $student_json_file=fopen($student_json_name,'r');
+        $student_json_data=fgets($student_json_file);
+        //echo $student_json_data;
+        fclose($student_json_file);
+        $student_json_data=substr($student_json_data,0,strlen($student_json_data)-1);
+        $student_obj=json_decode($student_json_data);
+        $student_ansflag=(int)$student_obj->{$question_id};
+        if($student_ansflag==1)
+        {
+            echo "daluole";
+        }
+        else
+        {
+            $false_Content = file_get_contents('wrong.html');
+            echo $false_Content;
+            $outPut .= "No";
+        }
+
 	}
 
 
